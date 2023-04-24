005068a2244d9ac8a3c1dd77e0de768a63ac0543
polarcube1 <polarcube1@gmail.com>
2023-04-24 17:55:36 -0400

    📦 Added Project

---
+++ scripts.js
@@ -0,0 +1,42 @@
+// scripts.js
+
+let mediaRecorder;
+let recordedBlobs;
+
+function startRecording() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            mediaRecorder = new MediaRecorder(stream);
+            recordedBlobs = [];
+
+            mediaRecorder.ondataavailable = event => {
+                if (event.data && event.data.size > 0) {
+                    recordedBlobs.push(event.data);
+                }
+            };
+
+            mediaRecorder.start();
+        })
+        .catch(error => {
+            console.error('Error accessing user media:', error);
+        });
+}
+
+function stopRecording() {
+    mediaRecorder.stop();
+
+    const blob = new Blob(recordedBlobs, { type: 'audio/webm' });
+    const recordingName = 'Recording-' + Math.floor(Math.random() * 10000);
+    const url = URL.createObjectURL(blob);
+    const recordingList = document.getElementById('recordingList');
+    const option = document.createElement('option');
+
+    option.value = url;
+    option.textContent = recordingName;
+    recordingList.appendChild(option);
+}
+
+function playRecording(url) {
+    const audio = new Audio(url);
+    audio.play();
+}
